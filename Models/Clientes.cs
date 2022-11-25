d6767251361fabb1a5389081bf14b84dc6d41db6
Jonathan3249 <jonathanmanfredo3259@gmail.com>
2022-11-24 20:58:29 -0600

    Agregar archivos de proyecto.

---
+++ Models/Clientes.cs
@@ -0,0 +1,16 @@
+﻿namespace Factura.Models
+{
+    public class Clientes
+    {
+        public int? idCliente { get; set; }
+
+        public string? nombreCompleto { get; set; }
+
+        public string? telefono { get; set; }
+
+        public string? numeroContador { get; set; }
+
+        public string? correo { get; set; }
+        public string? direccion { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+using Factura;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://proyectofactura.azurewebsites.net/") });
+
+await builder.Build().RunAsync();
